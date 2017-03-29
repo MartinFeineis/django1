4c305a2531e96274d30041a5d7edc170c82af887
Martin Feineis <mfeineis@point.io>
2017-03-29 15:47:06 -0400

    templates with index.html working

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -3,7 +3,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-	return HttpResponse("Rango says hello there <a href='/rango/about/'>rango about</a>")
+	contex_dict = {'boldmessage' : 'What is going on'}
+	return render(request, 'rango/index.html', context=contex_dict)
 
 def about(request):
 	return HttpResponse('Rango says here is the about hello <a href="/rango/">Index</a>')

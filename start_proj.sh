9f4f392f60715bd6a2f080151e36c96b2dea6cef
Martin Feineis <mfeineis@point.io>
2017-03-30 08:35:49 -0400

    start from rock-bottom

--- start_proj.sh
+++ start_proj.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 pyenv local 3.5.1
+git checkout 1a6fdc3a96d04065cf9b17f287001ce05183f701
 pip install django
 django-admin.py startproject tango_with_django_project
 cd tango_with_django_project
 python manage.py startapp rango
-#python manage.py  runserver
+python manage.py  runserver

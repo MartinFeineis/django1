4ccf1dc16a7ab06ea4c91f02fe4c14426731b59e
Martin Feineis <mfeineis@point.io>
2017-03-31 08:42:42 -0400

    fixed typo in STATICFILES_DIRS with the s at the end

--- start_proj.sh
+++ start_proj.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 pyenv local 3.5.1
 git checkout 1a6fdc3a96d04065cf9b17f287001ce05183f701
-pip install django
+pip install django==1.10.6
 django-admin.py startproject tango_with_django_project
 cd tango_with_django_project
 python manage.py startapp rango

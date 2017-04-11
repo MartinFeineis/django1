1b132d44a714ac030850b316647c0f36d42a516f
Martin Feineis <mfeineis@point.io>
2017-04-11 18:17:56 -0400

    added Exercise1 from Chapter 1

--- tango_with_django_project/rango/models.py
+++ tango_with_django_project/rango/models.py
@@ -2,6 +2,8 @@ from django.db import models
 
 class Category(models.Model):
 	name = models.CharField(max_length=128, unique=True)
+	views = models.IntegerField()
+	likes = models.IntegerField()
 
 	class Meta:
 		verbose_name_plural = 'Categories'

8b815446f7508acee7ce7ed3e6272fe5dfdaf730
Martin Feineis <mfeineis@point.io>
2017-05-10 06:59:16 -0400

    6.2 Showing Categories on Rango's Homepage

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
-
+from rango.models import Category
 from django.http import HttpResponse
 
 def index(request):
-	contex_dict = {'boldmessage' : 'What is going on'}
-	return render(request, 'rango/index.html', context=contex_dict)
+	#context_dict = {'boldmessage' : 'What is going on'}
+	category_list = Category.objects.order_by('-likes')[:5]
+	context_dict = {'categories' : category_list}
+	return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
 	return HttpResponse('Rango says here is the about hello <a href="/rango/">Index</a>')

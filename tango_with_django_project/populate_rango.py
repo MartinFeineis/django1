5badebc6689df35815389c01d0058945d24a4c65
Martin Feineis <mfeineis@point.io>
2017-09-24 16:43:30 -0400

    restored old populate

--- tango_with_django_project/populate_rango.py
+++ tango_with_django_project/populate_rango.py
@@ -9,59 +9,48 @@ from rango.models import Category, Page
 
 def populate():
     python_pages = [
-    {"title": "Official Python Tutorial", "url": "http://docs.python.org/2/tutorial/", "views": 23},
-    {"title": "How to Think like a Computer Scientist", "url": "http://www.greenteapress.com/thinkpython/", "views": 45},
-    {"title": "Learn Python in 10 Minutes", "url": "http://www.korokithakis.net/tutorials/python/", "views": 12}]
+    {"title": "Official Python Tutorial", "url": "http://docs.python.org/2/tutorial/"},
+    {"title": "How to Think like a Computer Scientist", "url": "http://www.greenteapress.com/thinkpython/"},
+    {"title": "Learn Python in 10 Minutes", "url": "http://www.korokithakis.net/tutorials/python/"}]
 
     django_pages = [
-    {"title": "Official Django Tutorial", "url": "https://docs.djangoproject", "views": 67},
-    {"title": "Django Rocks", "url" : "http://www.djangorocks.com/", "views": 50},
-    {"title": "How to Tango with Django", "url": "http://www.tangowithdjango.com/", "views": 10}]
+    {"title": "Official Django Tutorial", "url": "https://docs.djangoproject"},
+    {"title": "Django Rocks", "url" : "http://www.djangorocks.com/"},
+    {"title": "How to Tango with Django", "url": "http://www.tangowithdjango.com/"}]
 
     other_pages = [
-    {"title": "Bottle", "url": "http://bottlepy.org/docs/dev/", "views": 2},
-    {"title": "Flask", "url": "http://flask.pocoo.org", "views": 6}]
+    {"title": "Bottle", "url": "http://bottlepy.org/docs/dev/"},
+    {"title": "Flask", "url": "http://flask.pocoo.org"}]
 
     cats = {"Python": {"pages": python_pages, "views": 128, "likes": 64},
             "Django": {"pages": django_pages, "views": 64, "likes": 32},
             "Other Frameworks": {"pages": other_pages, "views": 32, "likes": 16}}
 
     for cat, cat_data in cats.items():
-<<<<<<< HEAD
+        print(str(cat)+ " cat")
+        print(type(cat_data["views"]), " Cat_Data")
         c = add_cat(cat, cat_data["views"], cat_data["likes"])
         for p in cat_data["pages"]:
-            add_page(c, p["title"], p["url"], p["views"])
-=======
-        print(str(cat)+ "cat")
-        print(str(cat_data) + "Cat_Data")
-        c = add_cat(cat, cat_data["views"], cat["likes"])
-        for p in cat_data["pages"]:
-            print(p + "this is p")
-            add_page(c, p["title"], p["url"]) #, p["views"], p["likes"])
->>>>>>> parent of 703adc1... database working
+            add_page(c, p["title"], p["url"])
 
     for c in Category.objects.all():
         for p in Page.objects.filter(category=c):
             print(" - {0} - {1}".format(str(c), str(p)))
 
 
-def add_page(cat, title, url, views):
-    p = Page.objects.get_or_create(category=cat, title=title, views=views)[0]
+def add_page(cat, title, url):
+    p = Page.objects.get_or_create(category=cat, title=title)[0]
     p.url = url
-    #p.views = views
     p.save()
     return p
 
 
 def add_cat(name, views=0, likes=0):
-<<<<<<< HEAD
     c = Category.objects.get_or_create(name=name, views=views, likes=likes)[0]
-=======
-    c = Category.objects.get_or_create(name=name)[0]
-    print(c)
->>>>>>> parent of 703adc1... database working
+    print(str(c) + " inside add_cat")
     c.views = views
     c.likes = likes
+    print(c.views, c.likes, " likes and views")
     c.save()
     return c
 

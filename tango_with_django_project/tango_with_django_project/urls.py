07e96eacc728cb8e97b1a9787906cd5c0459f40b
Martin Feineis <mfeineis@point.io>
2017-09-24 13:39:04 -0400

    wihtout appname

--- tango_with_django_project/tango_with_django_project/urls.py
+++ tango_with_django_project/tango_with_django_project/urls.py
@@ -21,7 +21,7 @@ from rango import views
 from django.conf import settings
 from django.conf.urls.static import static
 
-app_name = 'rango'
+#app_name = 'rango'
 urlpatterns = [
 	url(r'^$', views.index, name='index'),
 	url(r'^rango/', include('rango.urls')),

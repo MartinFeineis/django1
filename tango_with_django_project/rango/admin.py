bf7152e85d79d4a7db6bbcc81c7765205b96701e
MartinFeineis <martifein@gmail.com>
2019-02-02 22:08:24 -0500

    removed tango project

--- tango_with_django_project/rango/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from rango.models import Category, Page
-
-class CategoryAdmin(admin.ModelAdmin):
-	prepopulated_fields = {'slug':('name',)}
-
-admin.site.register(Category)
-admin.site.register(Page)

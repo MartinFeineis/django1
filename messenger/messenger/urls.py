08d397f2f604130e534a70fa3f23faec3fc7032c
MartinFeineis <martifein@gmail.com>
2019-04-20 20:17:14 -0400

    started REST framework

--- messenger/messenger/urls.py
+++ messenger/messenger/urls.py
@@ -16,7 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+#Rest Framework
+from rest_framework import routers
+from messenger.apiapp import views
+
 urlpatterns = [
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('apiapp/', include('apiapp.urls')),
     path('admin/', admin.site.urls),
 ]

af3835c72cdc5fe65d00240f4dadbe8abcd192b4
MartinFeineis <martifein@gmail.com>
2019-04-24 09:03:42 -0400

    fine grained Docker builds

--- build_d.sh
+++ build_d.sh
@@ -2,17 +2,24 @@
 BYellow='\033[1;33m'
 Color_Off='\033[0m'
 
+containerName=ConDjango
+ImageName=imgdjango
+
 if [ "$1" == "-f" ]; then
   echo -e " ${BYellow} Building new Image ${Color_Off}"
-  sudo docker build -t djangorest --no-cache --iidfile=image.id . #Force build new image
+  sudo docker build -t $ImageName --no-cache --iidfile=image.id . #Force build new image
 elif [ "$1" == "-s" ]; then
   echo -e " ${BYellow} Stopping Docker Containers ${Color_Off}"
-  sudo docker stop $(sudo docker ps -q) #stop all running docker containers
+  #sudo docker stop $(sudo docker ps -q) #stop all running docker containers
+  sudo docker stop $containerName #stop all running docker containers
+  echo -e " ${BYellow} Deleting Docker Containers ${Color_Off}"
+  #sudo docker stop $(sudo docker ps -q) #stop all running docker containers
+  sudo docker rm $containerName
 fi
-sudo docker build -t djangorest --iidfile=image.id .
+sudo docker build -t $ImageName --iidfile=image.id .
 #sudo docker tag $(cat image.id) djangoMar
 cat image.id
 echo -e " ${BYellow}  build Dockerfile and cating imagid ${Color_Off}"
 rm -f cont.id
 echo -e " ${BYellow}  deleting cont.id File and running container ${Color_Off}"
-sudo docker run -v $(pwd):/var/www/ -w /var/www/ -t $(cat image.id)  uwsgi --ini uwsgi.ini
+sudo docker run --name $containerName -v $(pwd):/var/www/ -w /var/www/ -t $(cat image.id)  uwsgi --ini uwsgi.ini
